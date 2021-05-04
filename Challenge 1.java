d4a101261f88b2730f212e03ebef1c40a2f20b72
María Campaña <71124586+Johari-lab@users.noreply.github.com>
2021-05-04 12:16:37 +0200

    Update Challenge 1.java

--- Challenge 1.java
+++ Challenge 1.java
@@ -15,5 +15,23 @@ class Main {
     max.maximum(list1); //5
     max.maximum(list2); //8
   }
+}
 
+class Maximum{
+  public Integer maximum(Integer[] numList) { 
+    //https://www.javatpoint.com/int-vs-integer-java
+
+    /*Let's create the maximum variable. Our guess is that the first integer on the numList is the largest*/
+    int maximum = numList[0];
+
+    /*Let's look at the rest of the numbers in numList one by one with the help of a for loop. For each one, we will test whether or not it's larger than our current guess of the largest number and if it is, we will make that number our new guess*/
+    for (int i : numList) {
+      if (i > maximum)
+      maximum = i;
+      }
+
+      System.out.println("Maximum integer in the list: " + maximum);
+      return maximum;
+      /*After looking at all the integers in numList, our most recent guess will be the actual largest number in the entire list*/
+  }
 }
